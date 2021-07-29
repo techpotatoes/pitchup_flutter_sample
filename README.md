18aba2ec2247d07fa19e62339b3921e5357eab42
Lucas Bento <lbento@tyro.com>
2021-07-30 00:04:10 +1000

    Implemented working sample

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # PitchUp Flutter Sample
 
-This app is a sample on how to use the Flutter library - link - TODO
+This app is a sample on how to use 2 dart libraries to implement a guitar tuner in Flutter - https://github.com/techpotatoes/pitchup-dart and https://github.com/techpotatoes/dartYIN
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_capture/flutter_audio_capture.dart';
+import 'package:pitch_detector_dart/pitch_detector.dart';
+import 'package:pitchupdart/instrument_type.dart';
+import 'package:pitchupdart/pitch_handler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,6 +35,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _audioRecorder = FlutterAudioCapture();
+  final pitchDetectorDart = PitchDetector(44100, 2000);
+  final pitchupDart = PitchHandler(InstrumentType.guitar);
+
+  var note = "";
+  var status = "Click on start";
+
+  Future<void> _startCapture() async {
+    await _audioRecorder.start(listener, onError,
+        sampleRate: 44100, bufferSize: 3000);
+
+    setState(() {
+      note = "";
+      status = "Play something";
+    });
+  }
+
+  Future<void> _stopCapture() async {
+    await _audioRecorder.stop();
+
+    setState(() {
+      note = "";
+      status = "Click on start";
+    });
+  }
+
+  void listener(dynamic obj) {
+    //Gets the audio sample
+    var buffer = Float64List.fromList(obj.cast<double>());
+    final List<double> audioSample = buffer.toList();
+
+    //Uses pitch_detector_dart library to detect a pitch from the audio sample
+    final result = pitchDetectorDart.getPitch(audioSample);
+
+    //If there is a pitch - evaluate it
+    if (result.pitched) {
+      //Uses the pitchupDart library to check a given pitch for a Guitar
+      final handledPitchResult = pitchupDart.handlePitch(result.pitch);
+
+      //Updates the state with the result
+      setState(() {
+        note = handledPitchResult.note;
+        status = handledPitchResult.tuningStatus.toString();
+      });
+    }
+  }
+
+  void onError(Object e) {
+    print(e);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,14 +93,39 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Nothing to see here',
-            ),
-          ],
-        ),
+        child: Column(children: [
+          Center(
+              child: Text(
+            note,
+            style: const TextStyle(
+                color: Colors.black87,
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold),
+          )),
+          const Spacer(),
+          Center(
+              child: Text(
+            status,
+            style: const TextStyle(
+                color: Colors.black87,
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold),
+          )),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                  child: Center(
+                      child: FloatingActionButton(
+                          onPressed: _startCapture,
+                          child: const Text("Start")))),
+              Expanded(
+                  child: Center(
+                      child: FloatingActionButton(
+                          onPressed: _stopCapture, child: const Text("Stop")))),
+            ],
+          ))
+        ]),
       ),
     );
   }
