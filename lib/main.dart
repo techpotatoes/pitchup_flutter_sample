876ab55824e9fdc6b702d8aac30c886a6c5e78be
Lucas <lucasbento7@gmail.com>
2024-04-04 11:53:46 +1000

    Refresh sample app

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,50 @@
-import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_audio_capture/flutter_audio_capture.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pitch_detector_dart/pitch_detector.dart';
 import 'package:pitchupdart/instrument_type.dart';
 import 'package:pitchupdart/pitch_handler.dart';
+import 'package:pitchupfluttersample/home/home_screen.dart';
+import 'package:pitchupfluttersample/home/pitch_cubit.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _audioRecorder = FlutterAudioCapture();
-  final pitchDetectorDart = PitchDetector(44100, 2000);
-  final pitchupDart = PitchHandler(InstrumentType.guitar);
-
-  var note = "";
-  var status = "Click on start";
-
-  Future<void> _startCapture() async {
-    await _audioRecorder.start(listener, onError,
-        sampleRate: 44100, bufferSize: 3000);
-
-    setState(() {
-      note = "";
-      status = "Play something";
-    });
-  }
-
-  Future<void> _stopCapture() async {
-    await _audioRecorder.stop();
-
-    setState(() {
-      note = "";
-      status = "Click on start";
-    });
-  }
-
-  void listener(dynamic obj) {
-    //Gets the audio sample
-    var buffer = Float64List.fromList(obj.cast<double>());
-    final List<double> audioSample = buffer.toList();
-
-    //Uses pitch_detector_dart library to detect a pitch from the audio sample
-    final result = pitchDetectorDart.getPitch(audioSample);
-
-    //If there is a pitch - evaluate it
-    if (result.pitched) {
-      //Uses the pitchupDart library to check a given pitch for a Guitar
-      final handledPitchResult = pitchupDart.handlePitch(result.pitch);
-
-      //Updates the state with the result
-      setState(() {
-        note = handledPitchResult.note;
-        status = handledPitchResult.tuningStatus.toString();
-      });
-    }
-  }
-
-  void onError(Object e) {
-    print(e);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(children: [
-          Center(
-              child: Text(
-            note,
-            style: const TextStyle(
-                color: Colors.black87,
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold),
-          )),
-          const Spacer(),
-          Center(
-              child: Text(
-            status,
-            style: const TextStyle(
-                color: Colors.black87,
-                fontSize: 14.0,
-                fontWeight: FontWeight.bold),
-          )),
-          Expanded(
-              child: Row(
-            children: [
-              Expanded(
-                  child: Center(
-                      child: FloatingActionButton(
-                          onPressed: _startCapture,
-                          child: const Text("Start")))),
-              Expanded(
-                  child: Center(
-                      child: FloatingActionButton(
-                          onPressed: _stopCapture, child: const Text("Stop")))),
-            ],
-          ))
-        ]),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<FlutterAudioCapture>(
+          create: (context) => FlutterAudioCapture(),
+        ),
+        RepositoryProvider<PitchDetector>(
+          create: (context) => PitchDetector(44100, 2000),
+        ),
+        RepositoryProvider<PitchHandler>(
+          create: (context) => PitchHandler(InstrumentType.guitar),
+        ),
+      ],
+      child: MultiBlocProvider(
+          providers: [
+            BlocProvider<PitchCubit>(
+              create: (context) => PitchCubit(
+                context.read<FlutterAudioCapture>(),
+                context.read<PitchDetector>(),
+                context.read<PitchHandler>(),
+              ),
+            ),
+          ],
+          child: MaterialApp(
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const HomeScreen(),
+          )
       ),
     );
   }
